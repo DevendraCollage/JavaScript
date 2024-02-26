7a14231450cf294f5b507533ac26cdec811a4e0c
Devendra Parmar <devendraparmarcollage@gmail.com>
2024-02-26 21:48:35 +0530

    modified:   6_Arrays.js

--- 6_Arrays.js
+++ 6_Arrays.js
@@ -371,3 +371,18 @@ const sum = sumOfNumbers.reduce((currElem, nextElem) => {
   return currElem + nextElem;
 });
 console.log(sum);
+
+//? Reduce method
+// The reduce method in JavaScript is used to accumulate or reduce an array to a single value. It iterates over the elements of an array and applies a callback function to each elements, updating an accumulator value with the result. Teh reduce method takes a callback function as its first argument and an optional initial value for the accumulator as the second argument.
+// syntax
+// array.reduce(function callback(accumulator, currentValue, index, array) {
+//   // Your logic here
+//   // Return the updated accumulator value
+// }, initialValue);
+
+// callback : A function that is called once for each element in the array.
+// accumulator : The accumulator result of the previous iterations.
+// currentValue : The current elements being processed in the array.
+// index (optional) : The index of the current elements being processed.
+// array (optional) : The array reduce was called upon.
+// initialValue (optional) : An initial value for the accumulator. If not provided, the first element of the array is used as the initial accumulator value.
